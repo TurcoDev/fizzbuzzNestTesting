46733ca8937dd5bcc34fef85e6f3ba378ed38b1d
Manuel Torres Gil <mtorres@ual.es>
2022-03-16 14:23:28 +0100

    Pruebas del controlador y de integración

--- src/fizzbuzz/fizzbuzz.controller.spec.ts
+++ src/fizzbuzz/fizzbuzz.controller.spec.ts
@@ -1,17 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FizzbuzzController } from './fizzbuzz.controller';
-import { FizzbuzzModule } from './fizzbuzz.module';
+import { FizzbuzzService } from './fizzbuzz.service';
 
 describe('FizzbuzzController', () => {
   let controller: FizzbuzzController;
+  let service: FizzbuzzService;
+
+  let mockedFizzBuzzValue = 'Buzz';
+  let mockFizzBuzzService = {
+    fizzbuzz: () => mockedFizzBuzzValue,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [FizzbuzzModule],
-      //controllers: [FizzbuzzController],
-    }).compile();
+      controllers: [FizzbuzzController],
+      providers: [FizzbuzzService],
+    })
+      .overrideProvider(FizzbuzzService)
+      .useValue(mockFizzBuzzService)
+      .compile();
 
     controller = module.get<FizzbuzzController>(FizzbuzzController);
+    service = module.get<FizzbuzzService>(FizzbuzzService);
+  });
+
+  it('should return the correct Fizz Buzz word according the introduced number (Using spyOn)', () => {
+    const result = 'Fizz';
+
+    const fizzbuzzSpy = jest.spyOn(service, 'fizzbuzz');
+    fizzbuzzSpy.mockImplementation(() => result);
+
+    expect(controller.fizzbuzz(3)).toBe(result);
+
+    fizzbuzzSpy.mockRestore();
+  });
+
+  it('should return the correct Fizz Buzz word according the introduced number (Using mocking de servicios)', () => {
+    expect(controller.fizzbuzz(5)).toBe(mockedFizzBuzzValue);
   });
 
   it('should be defined', () => {
